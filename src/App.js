5a4fc115e31918def8f289ef1f0c03ed9a3c54fb
azakharchuk <artemzakharchuk@tech-stack.io>
2023-10-10 17:53:18 +0300

    Make simple lazy loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 
@@ -5,14 +6,15 @@ import GlobalModalProvider from "./contexts/globalModalContext";
 import GlobalSpinnerProvider from "./contexts/globalSpinnerContext";
 import AuthProvider from "./contexts/authContext";
 
-import LoginPage from "./pages/LoginPage";
-import EmptyRoutePage from "./pages/EmptyRoutePage";
-import MainPage from "./pages/MainPage";
-import AccountPage from "./pages/AccountPage";
-
 import PrivateRoute from "./components/HOC/PrivateRoute";
 import GlobalModal from "./components/GlobalModal";
 import GlobalSpinner from "./components/GlobalSpinner";
+import Loading from "./components/Loading";
+
+const LoginPage = lazy(() => import("./pages/LoginPage.js"));
+const EmptyRoutePage = lazy(() => import("./pages/EmptyRoutePage.js"));
+const MainPage = lazy(() => import("./pages/MainPage.js"));
+const AccountPage = lazy(() => import("./pages/AccountPage.js"));
 
 export default function App() {
   return (
@@ -26,16 +28,24 @@ export default function App() {
               <Router>
                 <Switch>
                   <PrivateRoute path="/main">
-                    <MainPage />
+                    <Suspense fallback={<Loading />}>
+                      <MainPage />
+                    </Suspense>
                   </PrivateRoute>
                   <PrivateRoute path="/account">
-                    <AccountPage />
+                    <Suspense fallback={<Loading />}>
+                      <AccountPage />
+                    </Suspense>
                   </PrivateRoute>
                   <Route path="/login">
-                    <LoginPage />
+                    <Suspense fallback={<Loading />}>
+                      <LoginPage />
+                    </Suspense>
                   </Route>
                   <Route path="/">
-                    <EmptyRoutePage />
+                    <Suspense fallback={<Loading />}>
+                      <EmptyRoutePage />
+                    </Suspense>
                   </Route>
                 </Switch>
               </Router>
